b7fa82e649622de59516871d29545dbb62fa9519
Mohammad Amin Yaaghoubi <amin-1387@hotmail.com>
2019-12-28 13:26:14 +0330

    add basic files

---
+++ ForgBase/ForgLib/Base/Widgets/Menu/Tools/FrgBase_MenuTools.hxx
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef _FrgBase_MenuTools_Header
+#define _FrgBase_MenuTools_Header
+
+#include <FrgBase_Global.hxx>
+
+BeginForgBaseLib
+
+
+
+EndForgBaseLib
+
+#endif // !_FrgBase_MenuTools_Header
---
+++ ForgBase/ForgLib/Base/Widgets/Tree/FrgBase_Tree.hxx
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef _FrgBase_Tree_Header
+#define _FrgBase_Tree_Header
+
+#include <FrgBase_Global.hxx>
+
+
+
+#endif // !_FrgBase_Tree_Header
