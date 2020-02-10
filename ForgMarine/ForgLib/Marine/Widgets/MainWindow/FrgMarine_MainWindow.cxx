b858415c4e4ed556a865e2fa5f2d84275bcd6be7
aminyaa <ma.yaaghoubi@gmail.com>
2020-02-11 01:04:52 +0330

    add tabwidget to view scenes and some minor changes

--- ForgMarine/ForgLib/Marine/Widgets/MainWindow/FrgMarine_MainWindow.cxx
+++ ForgMarine/ForgLib/Marine/Widgets/MainWindow/FrgMarine_MainWindow.cxx
@@ -1,12 +1,15 @@
 #include <FrgMarine_MainWindow.hxx>
 #include <FrgMarine_Tree.hxx>
 #include <FrgBase_PropertiesPanel.hxx>
+#include <FrgBase_TabWidget.hxx>
 
 #include <QtWidgets/QDockWidget>
 
 ForgMarineLib::FrgMarine_MainWindow::FrgMarine_MainWindow(QWidget* parent)
 	: FrgBase_MainWindow(parent)
 {
+	this->setMinimumSize(QSize(1366, 768));
+
 	InitMainWindow();
 }
 
@@ -29,4 +32,24 @@ void ForgMarineLib::FrgMarine_MainWindow::InitMainWindow()
 	this->addDockWidget(Qt::DockWidgetArea::LeftDockWidgetArea, thePropertiesPanelDockWidget_);
 
 	CreateConsoleOutput();
+
+	theTabWidget_ = new ForgBaseLib::FrgBase_TabWidget(this);
+	this->setCentralWidget(theTabWidget_);
+}
+
+void ForgMarineLib::FrgMarine_MainWindow::ShowTabWidget(QWidget * widget, const QString& title)
+{
+	theTabWidget_->ShowTabWidget(widget, title);
+}
+
+void ForgMarineLib::FrgMarine_MainWindow::SetTabText(QWidget * widget, const QString & title)
+{
+	int index = theTabWidget_->indexOf(widget);
+
+	SetTabText(index, title);
+}
+
+void ForgMarineLib::FrgMarine_MainWindow::SetTabText(int index, const QString & title)
+{
+	theTabWidget_->setTabText(index, title);
 }
--- ForgMarine/ForgLib/Marine/Widgets/MainWindow/FrgMarine_MainWindow.hxx
+++ ForgMarine/ForgLib/Marine/Widgets/MainWindow/FrgMarine_MainWindow.hxx
@@ -5,6 +5,11 @@
 #include <FrgMarine_Global.hxx>
 #include <FrgBase_MainWindow.hxx>
 
+namespace ForgBaseLib
+{
+	class FrgBase_TabWidget;
+}
+
 BeginForgMarineLib
 
 class FORGMARINE_EXPORT FrgMarine_MainWindow
@@ -16,9 +21,19 @@ public:
 
 	FrgMarine_MainWindow(QWidget* parent = NullPtr);
 
+	ForgBaseLib::FrgBase_TabWidget* GetTabWidget() const { return theTabWidget_; }
+
+	void ShowTabWidget(QWidget* widget, const QString& title);
+	void SetTabText(QWidget* widget, const QString& title);
+	void SetTabText(int index, const QString& title);
+
 protected:
 
 	void InitMainWindow() override;
+
+private:
+
+	ForgBaseLib::FrgBase_TabWidget* theTabWidget_ = NullPtr;
 };
 
 EndForgMarineLib
