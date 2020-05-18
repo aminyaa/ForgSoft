139159700764937c4f196deb63787af6491e42d2
aminyaa <ma.yaaghoubi@gmail.com>
2020-05-18 16:49:01 +0430

    serialization specialization of some data types including: - vtkChartXY - vtkContextView - vtkGenericOpenGL RenderWindow - vtkTable

---
+++ ForgVisual/ForgLib/Visual/Serialization/Specialization/vtkGenericOpenGLRenderWindow/FrgVisual_SerialSpec_vtkGenericOpenGLRenderWindow.hxx
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef _FrgVisual_SerialSpec_vtkGenericOpenGLRenderWindow_Header
+#define _FrgVisual_SerialSpec_vtkGenericOpenGLRenderWindow_Header
+
+//#include <FrgVisual_Serialization_Global.hxx>
+//
+//#include <vtkGenericOpenGLRenderWindow.h>
+//
+//BOOST_SERIALIZATION_SPLIT_FREE(vtkGenericOpenGLRenderWindow)
+//
+//namespace boost
+//{
+//	namespace serialization
+//	{
+//		template<class Archive>
+//		void save(Archive& ar, const vtkGenericOpenGLRenderWindow& m, const unsigned int version)
+//		{
+//
+//		}
+//
+//		template<class Archive>
+//		void load(Archive& ar, vtkGenericOpenGLRenderWindow& m, const unsigned int version)
+//		{
+//
+//		}
+//	}
+//}
+//
+//SERIALIZE_VTKSMARTPOINTER(vtkGenericOpenGLRenderWindow)
+
+#endif // !_FrgVisual_SerialSpec_vtkGenericOpenGLRenderWindow_Header
