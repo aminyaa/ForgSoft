cd8e5eb16a49d31354603dfc2f6356b4d1b3ff43
aminyaa <ma.yaaghoubi@gmail.com>
2020-11-20 18:11:51 +0330

    changes to ForgVisual project

---
+++ ForgVisual/ForgLib/Visual/Serialization/Specialization/vtkProperty/FrgVisual_SerialSpec_vtkProperty.cxx
@@ -0,0 +1,13 @@
+#include <FrgVisual_SerialSpec_vtkProperty.hxx>
+
+DECLARE_SAVE_IMP_SPEC(vtkProperty)
+{
+
+}
+
+DECLARE_LOAD_IMP_SPEC(vtkProperty)
+{
+
+}
+
+BOOST_CLASS_EXPORT_CXX_SPEC(vtkProperty)
