b08ab99e0b5fcb6b9d1f3a0f7f89232f9dcd1116
aminyaa <ma.yaaghoubi@gmail.com>
2020-11-20 17:54:38 +0330

    add darkstyle class

---
+++ ForgBase/ForgLib/Base/Widgets/Styles/DarkStyle/FrgBase_StylesDarkStyle.hxx
@@ -0,0 +1,35 @@
+#pragma once
+#ifndef _FrgBase_StylesDarkStyle_Header
+#define _FrgBase_StylesDarkStyle_Header
+
+#include <FrgBase_Global.hxx>
+
+#include <QApplication>
+#include <QFile>
+#include <QFont>
+#include <QProxyStyle>
+#include <QStyleFactory>
+
+BeginForgBaseLib
+
+class FORGBASE_EXPORT FrgBase_StylesDarkStyle
+	: public QProxyStyle
+{
+	Q_OBJECT
+
+public:
+	FrgBase_StylesDarkStyle();
+	explicit FrgBase_StylesDarkStyle(QStyle* style);
+
+	QStyle* baseStyle() const;
+
+	void polish(QPalette& palette) override;
+	void polish(QApplication* app) override;
+
+private:
+	QStyle* styleBase(QStyle* style = Q_NULLPTR) const;
+};
+
+EndForgBaseLib
+
+#endif // !_FrgBase_StylesDarkStyle_Header
