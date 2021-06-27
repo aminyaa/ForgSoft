8097a3bd69fe5802a02eae9a459927cfa99c061c
aminyaa <ma.yaaghoubi@gmail.com>
2021-06-27 23:43:49 +0430

    add application and handling structured exceptions.

--- ForgBase/ForgLib/Base/Core/Application/FrgBase_Application.cxx
+++ ForgBase/ForgLib/Base/Core/Application/FrgBase_Application.cxx
@@ -1,54 +1,124 @@
-//#include <FrgBase_Application.hxx>
-//#include <FrgBase_ApplicationCore.hxx>
-//#include <FrgBase_MainWindow.hxx>
-//
-//#include <QtWidgets/QMainWindow>
-//#include <QtWidgets/QSplashScreen>
-//#include <QtWidgets/QApplication>
-//
-//ForgBaseLib::FrgBase_Application::FrgBase_Application()
-//{
-//
-//}
-//
-//ForgBaseLib::FrgBase_Application::~FrgBase_Application()
-//{
-//	delete theSplashScreen_;
-//	theSplashScreen_ = nullptr;
-//
-//	delete theMainWindow_;
-//	theMainWindow_ = nullptr;
-//
-//	delete theAppCore_;
-//	theAppCore_ = nullptr;
-//}
-//
-//ForgBaseLib::FrgBase_Application::Status ForgBaseLib::FrgBase_Application::Initialize(int argc, char * argv[])
-//{
-//	theAppCore_ = new FrgBase_ApplicationCore(argc, argv);
-//
-//	QApplication::instance()->installEventFilter(theAppCore_);
-//
-//#if 0
-//	pqSettings *settings = this->PVApp->settings();
-//	if (this->PVApp->getOptions()->GetDisableRegistry() == false &&
-//		settings->value("GeneralSettings.ShowSplashScreen", true).toBool())
-//	{
-//		// Create and show the splash screen as the main window is being created.
-//		QPixmap pixmap("");
-//		this->Splash = new QSplashScreen(pixmap, Qt::WindowStaysOnTopHint);
-//		this->Splash->setMask(pixmap.createMaskFromColor(QColor(Qt::transparent)));
-//		this->Splash->show();
-//	}
-//#endif
-//
-//	// Create main window.
-//	theMainWindow_ = new FrgBase_MainWindow;
-//
-//	theMainWindow_->show();
-//	if (this->theSplashScreen_)
-//	{
-//		this->theSplashScreen_->finish(this->theMainWindow_);
-//	}
-//	return RunApplication;
-//}
+#include <FrgBase_Application.hxx>
+#include <FrgBase_MainWindow.hxx>
+
+#include <windows.h>
+#include <eh.h>
+#include <Psapi.h>
+#include <string>
+#include <sstream>
+
+class InfoFromSE
+{
+public:
+	typedef unsigned int exception_code_t;
+
+	static const char* opDescription(const ULONG opcode)
+	{
+		switch (opcode) {
+		case 0: return "read";
+		case 1: return "write";
+		case 8: return "user-mode data execution prevention (DEP) violation";
+		default: return "unknown";
+		}
+	}
+
+	static const char* seDescription(const exception_code_t& code)
+	{
+		switch (code) {
+		case EXCEPTION_ACCESS_VIOLATION:         return "EXCEPTION_ACCESS_VIOLATION";
+		case EXCEPTION_ARRAY_BOUNDS_EXCEEDED:    return "EXCEPTION_ARRAY_BOUNDS_EXCEEDED";
+		case EXCEPTION_BREAKPOINT:               return "EXCEPTION_BREAKPOINT";
+		case EXCEPTION_DATATYPE_MISALIGNMENT:    return "EXCEPTION_DATATYPE_MISALIGNMENT";
+		case EXCEPTION_FLT_DENORMAL_OPERAND:     return "EXCEPTION_FLT_DENORMAL_OPERAND";
+		case EXCEPTION_FLT_DIVIDE_BY_ZERO:       return "EXCEPTION_FLT_DIVIDE_BY_ZERO";
+		case EXCEPTION_FLT_INEXACT_RESULT:       return "EXCEPTION_FLT_INEXACT_RESULT";
+		case EXCEPTION_FLT_INVALID_OPERATION:    return "EXCEPTION_FLT_INVALID_OPERATION";
+		case EXCEPTION_FLT_OVERFLOW:             return "EXCEPTION_FLT_OVERFLOW";
+		case EXCEPTION_FLT_STACK_CHECK:          return "EXCEPTION_FLT_STACK_CHECK";
+		case EXCEPTION_FLT_UNDERFLOW:            return "EXCEPTION_FLT_UNDERFLOW";
+		case EXCEPTION_ILLEGAL_INSTRUCTION:      return "EXCEPTION_ILLEGAL_INSTRUCTION";
+		case EXCEPTION_IN_PAGE_ERROR:            return "EXCEPTION_IN_PAGE_ERROR";
+		case EXCEPTION_INT_DIVIDE_BY_ZERO:       return "EXCEPTION_INT_DIVIDE_BY_ZERO";
+		case EXCEPTION_INT_OVERFLOW:             return "EXCEPTION_INT_OVERFLOW";
+		case EXCEPTION_INVALID_DISPOSITION:      return "EXCEPTION_INVALID_DISPOSITION";
+		case EXCEPTION_NONCONTINUABLE_EXCEPTION: return "EXCEPTION_NONCONTINUABLE_EXCEPTION";
+		case EXCEPTION_PRIV_INSTRUCTION:         return "EXCEPTION_PRIV_INSTRUCTION";
+		case EXCEPTION_SINGLE_STEP:              return "EXCEPTION_SINGLE_STEP";
+		case EXCEPTION_STACK_OVERFLOW:           return "EXCEPTION_STACK_OVERFLOW";
+		default: return "UNKNOWN EXCEPTION";
+		}
+	}
+
+	static std::string information(struct _EXCEPTION_POINTERS* ep, bool has_exception_code = false, exception_code_t code = 0)
+	{
+		HMODULE hm;
+		::GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, static_cast<LPCTSTR>(ep->ExceptionRecord->ExceptionAddress), &hm);
+		MODULEINFO mi;
+		::GetModuleInformation(::GetCurrentProcess(), hm, &mi, sizeof(mi));
+		char fn[MAX_PATH];
+		::GetModuleFileNameExA(::GetCurrentProcess(), hm, fn, MAX_PATH);
+
+		std::ostringstream oss;
+		oss << "SE " << (has_exception_code ? seDescription(code) : "") << " at address 0x" << std::hex << ep->ExceptionRecord->ExceptionAddress << std::dec
+			<< " inside " << fn << " loaded at base address 0x" << std::hex << mi.lpBaseOfDll << "\n";
+
+		if (has_exception_code && (
+			code == EXCEPTION_ACCESS_VIOLATION ||
+			code == EXCEPTION_IN_PAGE_ERROR)) {
+			oss << "Invalid operation: " << opDescription(ep->ExceptionRecord->ExceptionInformation[0]) << " at address 0x" << std::hex << ep->ExceptionRecord->ExceptionInformation[1] << std::dec << "\n";
+		}
+
+		if (has_exception_code && code == EXCEPTION_IN_PAGE_ERROR) {
+			oss << "Underlying NTSTATUS code that resulted in the exception " << ep->ExceptionRecord->ExceptionInformation[2] << "\n";
+		}
+
+		return oss.str();
+	}
+};
+
+#include <iostream>
+#include <exception>
+
+void translator(InfoFromSE::exception_code_t code, struct _EXCEPTION_POINTERS* ep)
+{
+	throw std::exception(InfoFromSE::information(ep, true, code).c_str());
+}
+
+ForgBaseLib::FrgBase_Application::FrgBase_Application
+(
+	int& argc,
+	char** argv,
+	int af
+)
+	: QApplication(argc, argv, af)
+{
+
+}
+
+bool ForgBaseLib::FrgBase_Application::notify(QObject* receiver, QEvent* event)
+{
+	CallTranslator();
+
+	bool done = true;
+	try
+	{
+		done = QApplication::notify(receiver, event);
+	}
+	catch (const std::exception& ex)
+	{
+		if (theParentMainWindow_)
+			theParentMainWindow_->PrintErrorToConsole(QString::fromStdString(ex.what()));
+	}
+	catch (...)
+	{
+		if (theParentMainWindow_)
+			theParentMainWindow_->PrintErrorToConsole("An Error Has Been Occurred.");
+	}
+
+	return done;
+}
+
+void ForgBaseLib::FrgBase_Application::CallTranslator() const
+{
+	_set_se_translator(translator);
+}
--- ForgBase/include/FrgBase_Application.hxx
+++ ForgBase/include/FrgBase_Application.hxx
@@ -1,41 +1,47 @@
-//#pragma once
-//#ifndef _FrgBase_Application_Header
-//#define _FrgBase_Application_Header
-//
-//#include <FrgBase_Global.hxx>
-//
-//class QMainWindow;
-//class QSplashScreen;
-//
-//BeginForgBaseLib
-//
-//class FrgBase_ApplicationCore;
-//
-//class FORGBASE_EXPORT FrgBase_Application
-//{
-//
-//public:
-//
-//	enum Status
-//	{
-//		ExitSuccess,
-//		ExitFailure,
-//		RunApplication
-//	};
-//
-//	FrgBase_Application();
-//	~FrgBase_Application();
-//
-//	// Initialize ParaView. It returns false if the initialization failed.
-//	Status Initialize(int argc, char* argv[]);
-//
-//private:
-//
-//	FrgBase_ApplicationCore* theAppCore_ = nullptr;
-//	QMainWindow* theMainWindow_ = nullptr;
-//	QSplashScreen* theSplashScreen_ = nullptr;
-//};
-//
-//EndForgBaseLib
-//
-//#endif // !_FrgBase_Application_Header
+#pragma once
+#ifndef _FrgBase_Application_Header
+#define _FrgBase_Application_Header
+
+#include <FrgBase_Global.hxx>
+#include <QtWidgets/QApplication>
+
+namespace ForgBaseLib
+{
+	class FrgBase_MainWindow;
+}
+
+BeginForgBaseLib
+
+class FORGBASE_EXPORT FrgBase_Application
+	: public QApplication
+{
+
+	Q_OBJECT
+
+public:
+
+	FrgBase_Application
+	(
+		int& argc,
+		char** argv,
+		int af = ApplicationFlags
+	);
+
+	bool notify(QObject* receiver, QEvent* event) override;
+
+	ForgBaseLib::FrgBase_MainWindow* GetParentMainWindow() const { return theParentMainWindow_; }
+	void SetParentMainWindow(ForgBaseLib::FrgBase_MainWindow* parentMainWindow) { theParentMainWindow_ = parentMainWindow; }
+
+protected:
+
+	// to catch structured exceptions you should call this function while overriding notify(QObject* receiver, QEvent* event)
+	virtual void CallTranslator() const;
+
+protected:
+
+	ForgBaseLib::FrgBase_MainWindow* theParentMainWindow_ = nullptr;
+};
+
+EndForgBaseLib
+
+#endif // !_FrgBase_Application_Header
