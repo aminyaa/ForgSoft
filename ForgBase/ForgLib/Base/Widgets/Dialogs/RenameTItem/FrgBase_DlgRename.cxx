039ee4a0dfe9848b9631c329f5f130301c2a238f
aminyaa <ma.yaaghoubi@gmail.com>
2023-03-12 14:05:33 +0330

    add validation method to rename tree item

--- ForgBase/ForgLib/Base/Widgets/Dialogs/RenameTItem/FrgBase_DlgRename.cxx
+++ ForgBase/ForgLib/Base/Widgets/Dialogs/RenameTItem/FrgBase_DlgRename.cxx
@@ -57,12 +57,15 @@ void ForgBaseLib::FrgBase_DlgRename::setupLayout()
 	QObject::connect(theOKButton_, SIGNAL(clicked()), this, SLOT(onOK()));
 	QObject::connect(theCloseButton_, SIGNAL(clicked()), this, SLOT(reject()));
 	QObject::connect(theNameLineEdit_, SIGNAL(returnPressed()), this, SLOT(onOK()));
+	QObject::connect(theNameLineEdit_, SIGNAL(textChanged(const QString&)), this, SLOT(TextChangedSlot(const QString&)));
 
 	theOKButton_->setFocus();
 	theNameLineEdit_->setFocus();
 	this->setModal(FrgTrue);
 
 	this->adjustSize();
+
+	theNameLineEdit_->textChanged(theNameLineEdit_->text());
 }
 
 void ForgBaseLib::FrgBase_DlgRename::keyPressEvent(QKeyEvent* event)
@@ -80,10 +83,86 @@ QString ForgBaseLib::FrgBase_DlgRename::GetLineEditName() const
 	return theNameLineEdit_->text();
 }
 
+bool ForgBaseLib::FrgBase_DlgRename::IsNameValid
+(
+	const QString& text
+)
+{
+	if (text.isEmpty())
+		return false;
+
+	for (const auto c : text)
+	{
+		if (!IsCharValid(c))
+			return false;
+	}
+
+	return true;
+}
+
+bool ForgBaseLib::FrgBase_DlgRename::IsCharValid
+(
+	const QChar myChar
+)
+{
+	if (myChar.isNumber())
+		return true;
+	if (myChar.isSpace())
+		return true;
+	if (myChar == '_')
+		return true;
+	if (myChar == ".")
+		return true;
+	if (myChar == "(")
+		return true;
+	if (myChar == ")")
+		return true;
+
+	const auto i = (int)myChar.toLatin1();
+	if (i >= 65 && i <= 90)
+		return true;
+	if (i >= 97 && i <= 122)
+		return true;
+
+	return false;
+}
+
 void ForgBaseLib::FrgBase_DlgRename::onOK()
 {
 	if (theNameLineEdit_->text().simplified() == "")
 		return;
 
-	accept();
+	if(theOKButton_->isEnabled())
+		accept();
+}
+
+void ForgBaseLib::FrgBase_DlgRename::TextChangedSlot
+(
+	const QString&
+)
+{
+	QString color = "black";
+
+	if (GetParentMainWindow())
+	{
+		if (GetParentMainWindow()->IsThemeDark())
+			color = "white";
+		else
+			color = "black";
+	}
+
+	if (!IsNameValid(theNameLineEdit_->text()))
+	{
+		theOKButton_->setEnabled(false);
+
+		color = "red";
+	}
+	else
+	{
+		theOKButton_->setEnabled(true);
+	}
+
+	QString style = "color: " + color + ";";
+
+	theNameLineEdit_->setStyleSheet(style);
 }
